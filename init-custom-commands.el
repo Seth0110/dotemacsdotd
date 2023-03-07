e3c43cef5761642bd88e3e586e40670939490b01
Seth Sevier <s.sevier@cryomech.com>
2023-03-07 11:10:49 -0500

    Latest tweaks

--- init-custom-commands.el
+++ init-custom-commands.el
@@ -1,3 +1,10 @@
-(defun usr/reload ()
+(defun reload ()
     (interactive)
     (load-file "~/.emacs.d/init.el"))
+
+(defun ask-before-closing ()
+  "Ask if you really want to quit"
+  (interactive)
+  (if (y-or-n-p (format "Are you sure you want to blaspheme the sacred editor? "))
+      (save-buffers-kill-emacs)                                                                                          (message "That's what I thought.")))
+(global-set-key (kbd "C-x C-c") 'ask-before-closing)
--- init-package.el
+++ init-package.el
@@ -1,44 +1,47 @@
 ;; Package management
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; Import packages
 (use-package ace-jump-mode)
 (use-package bison-mode)
 (use-package csharp-mode)
-(use-package docker)
 (use-package dockerfile-mode)
+(use-package elfeed)
+(use-package flycheck)
+(use-package flycheck-haskell)
 (use-package geiser)
-(use-package geiser-mit)
 (use-package gnuplot-mode)
 (use-package haskell-mode)
 (use-package json-mode)
+(use-package magit)
 (use-package markdown-mode)
-(use-package pdf-tools
-  (pdf-tools-install)
-  (setq-default pdf-view-display-size 'fit-width)
-  (add-hook 'pdf-view-mode (lambda () (display-line-numbers-mode 0))))
-(use-package racket-mode)
+(use-package pdf-tools)
+(use-package pomidor
+  :config (setq pomidor-sound-tick nil
+                pomidor-sound-tack nil))
 (use-package shakespeare-mode)
 (use-package srcery-theme)
+(use-package uptimes)
 (use-package visual-regexp)
 (use-package visual-regexp-steroids)
 (use-package yaml-mode)
 
+;; Load theme
 (if (display-graphic-p)
-    (load-theme 'srcery 1))
+    (load-theme 'solarized-selenized-light 1))
+
+;; Ace jump mode config
 (global-set-key (kbd "C-c ,") 'ace-jump-mode)
 
-;; pdf-tools as default PDF viewer
+;; Set pdf-tools as the default PDF viewer
 (setq TeX-view-program-selection '((output-pdf "PDF Tools"))
       TeX-view-program-list '(("PDF Tools" TeX-pdf-tools-sync-view))
       TeX-source-correlate-start-server t)
 (add-hook 'TeX-after-compilation-finished-functions
           #'TeX-revert-document-buffer)
-
 (pdf-tools-install)
