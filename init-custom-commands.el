8dc80910bbbd0079d2b104fcf2a3dfbb99f79395
Seth <seth.sevier@gmail.com>
2023-03-08 20:02:20 -0500

    light and dark mode commands

--- init-custom-commands.el
+++ init-custom-commands.el
@@ -8,3 +8,14 @@
   (if (y-or-n-p (format "Are you sure you want to blaspheme the sacred editor? "))
       (save-buffers-kill-emacs)                                                                                          (message "That's what I thought.")))
 (global-set-key (kbd "C-x C-c") 'ask-before-closing)
+
+(defun dark ()
+  (interactive)
+  (load-theme 'srcery))
+
+(defun light ()
+  (interactive)
+  (progn
+   (disable-theme 'srcery)
+   (if (eq system-type 'gnu/linux)
+       (set-frame-font "CMU Typewriter Text 18" nil t))))
