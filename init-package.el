438c5b127f9d05ff6c0944aca2ac87c7451bf9f7
Seth Sevier <s.sevier@cryomech.com>
2023-03-10 15:35:04 -0500

    Better use-package use, added nifty new packages, tons of org mode stuff, fixed custom set variables.

--- init-package.el
+++ init-package.el
@@ -6,45 +6,52 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(defun set-pdf-tools ()
+  "Set pdf-tools as the default PDF viewer"
+  (if (eq system-type 'gnu/linux)
+      (progn
+	(setq TeX-view-program-selection '((output-pdf "PDF Tools"))
+	      TeX-view-program-list '(("PDF Tools" TeX-pdf-tools-sync-view))
+	      TeX-source-correlate-start-server t)
+	(add-hook 'TeX-after-compilation-finished-functions
+		  #'TeX-revert-document-buffer)
+	(pdf-tools-install))))
+
 ;; Import packages
-(use-package ace-jump-mode)
+(use-package ace-jump-mode
+  :bind ("C-." . ace-jump-mode))
 (use-package bison-mode)
 (use-package csharp-mode)
+(use-package csv-mode
+  :init
+  (add-hook 'csv-mode 'csv-align-mode))
 (use-package dockerfile-mode)
 (use-package elfeed)
+(use-package elfeed-org
+  :config
+  (elfeed-org)
+  (setq rmh-elfeed-org-files (list "~/Documents/org/elfeed.org")))
+(use-package esh-autosuggest)
+(use-package eshell-syntax-highlighting)
 (use-package flycheck)
 (use-package flycheck-haskell)
 (use-package geiser)
 (use-package gnuplot-mode)
 (use-package haskell-mode)
 (use-package json-mode)
-(use-package magit)
 (use-package markdown-mode)
-(use-package pdf-tools)
-(use-package pomidor
-  :config (setq pomidor-sound-tick nil
-                pomidor-sound-tack nil))
+(use-package pdf-tools
+  :config (set-pdf-tools))
+(use-package rainbow-delimiters
+  :config
+  :hook emacs-lisp-mode-hook)
 (use-package shakespeare-mode)
-(use-package solarized-theme)
 (use-package srcery-theme)
+(use-package pomidor
+  :bind (("<f12>" . pomidor))
+  :config (setq pomidor-sound-tick nil
+		pomidor-sound-tack nil))
 (use-package uptimes)
 (use-package visual-regexp)
 (use-package visual-regexp-steroids)
 (use-package yaml-mode)
-
-;; Load theme
-(if (display-graphic-p)
-    (load-theme 'solarized-selenized-light 1))
-
-;; Ace jump mode config
-(global-set-key (kbd "C-c ,") 'ace-jump-mode)
-
-;; Set pdf-tools as the default PDF viewer
-(if (eq system-type 'gnu/linux)
-    (progn
-      (setq TeX-view-program-selection '((output-pdf "PDF Tools"))
-	    TeX-view-program-list '(("PDF Tools" TeX-pdf-tools-sync-view))
-	    TeX-source-correlate-start-server t)
-      (add-hook 'TeX-after-compilation-finished-functions
-		#'TeX-revert-document-buffer)
-      (pdf-tools-install)))
--- init.el
+++ init.el
@@ -20,8 +20,6 @@
 (column-number-mode t)
 (cond ((display-graphic-p)
        (global-hl-line-mode 1)))
-;; (add-hook 'text-mode-hook 'display-line-numbers-mode)
-;; (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 (global-display-line-numbers-mode 1)
 (setq display-line-numbers-type 'relative)
 
@@ -54,38 +52,16 @@
 (setq abbrev-file-name
       "~/.emacs.d/abbrev_defs")
 (setq eww-search-prefix "https://duckduckgo.com/lite/?q=")
+(setq browse-url-browser-function 'eww-browse-url)
 ;; (setq inhibit-startup-screen t)
 
+;; I hate custom set variables!!!
+(setq custom-file null-device)
+
 ;; Load more files
-(load-file "~/.emacs.d/init-skeleton.el")
 (load-file "~/.emacs.d/init-package.el")
+(load-file "~/.emacs.d/init-org.el")
+(load-file "~/.emacs.d/init-skeleton.el")
 (load-file "~/.emacs.d/init-custom-commands.el")
 (if (file-exists-p "init-work.el")
     (load-file "~/.emacs.d/init-work.el"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "f5b6be56c9de9fd8bdd42e0c05fecb002dedb8f48a5f00e769370e4517dde0e8" "57a29645c35ae5ce1660d5987d3da5869b048477a7801ce7ab57bfb25ce12d3e" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "623e9fe0532cc3a0bb31929b991a16f72fad7ad9148ba2dc81e395cd70afc744" "a24e023c71f74e8c6a79068947d706f68a566b49a774096671a5cbfe1fb90fc6" "8da34297ccd16aa9fdf75596dc06d519a5f036179fbff95107bbecdaadf965c4" "c5a81a42df109b02a9a68dfe0ed530080372c1a0bbcb374da77ee3a57e1be719" "e1498b2416922aa561076edc5c9b0ad7b34d8ff849f335c13364c8f4276904f0" "529c211e86eadecb67b6b64ffdf73e71c4337070bd9b3de053f8f7c5da9e07a2" default))
- '(display-line-numbers t)
- '(elfeed-feeds
-   '("https://lukesmith.xyz/index.xml" "https://www.youtube.com/feeds/videos.xml?channel_id=UCldfgbzNILYZA4dmDt4Cd6A" "https://www.youtube.com/feeds/videos.xml?channel_id=UCDRIjKy6eZOvKtOELtTdeUA"))
- '(ignored-local-variable-values
-   '((haskell-process-use-ghci . t)
-     (haskell-indent-spaces . 4)))
- '(package-selected-packages
-   '(display-wttr wttrin pomidor empv emms eww-lnum unicode-fonts hasklig-mode pdf-tools mines afternoon-theme elf-mode advice-patch adjust-parens adaptive-wrap ada-ref-man inkpot-theme ada-mode ack ace-window sokoban clojure-mode docker yaml-mode visual-regexp-steroids visual-regexp markdown-mode json-mode haskell-mode gnuplot-mode geiser-mit geiser elm-mode dockerfile-mode ace-jump-mode use-package))
- '(safe-local-variable-values
-   '((vc-prepare-patches-separately)
-     (diff-add-log-use-relative-names . t)
-     (vc-git-annotate-switches . "-w")))
- '(warning-suppress-types '((use-package) (emacs))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
